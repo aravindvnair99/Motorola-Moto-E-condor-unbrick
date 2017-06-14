49eacf6de7b5157e010eb9e9fadfbb85b58d4bf4
Aravind V. Nair <aravindv.nair1999@gmail.com>
2017-06-14 15:42:21 +0530

    Add files via upload

---
+++ Fastboot Unbrick.bat
@@ -0,0 +1,14 @@
+ECHO
+fastboot flash boot boot.img
+fastboot flash recovery recovery.img
+fastboot flash system system.img_sparsechunk.0
+fastboot flash system system.img_sparsechunk.1
+fastboot flash system system.img_sparsechunk.2
+fastboot flash modem NON-HLOS.bin
+fastboot erase modemst1
+fastboot erase modemst2
+fastboot flash fsg fsg.mbn
+fastboot erase cache
+fastboot erase userdata
+fastboot reboot
+PAUSE
